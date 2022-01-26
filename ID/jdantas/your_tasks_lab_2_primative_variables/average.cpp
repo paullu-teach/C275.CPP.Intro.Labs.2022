ed9da4d44e31b8990052cfcf8a34995e5e42b5d2
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2022-01-25 18:37:35 -0700

    Add files via upload

---
+++ ID/jdantas/your_tasks_lab_2_primative_variables/average.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // Task 1: Average
+
+    int number_1, number_2, number_3; // We initialize three numbers
+    cin >> number_1 >> number_2 >> number_3;
+
+    cout << float (number_1 + number_2 + number_3) / 3; // we output the average
+    // of the three numbers
+    return 0;
+
+}
+
---
+++ ID/jdantas/your_tasks_lab_2_primative_variables/exercise_float_and_integer_division.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+// Exercise "Float and Integer Division"
+
+    cout << 3/2 << endl;
+    cout << 3.0/2 << endl;
+    cout << double (3) /2 << endl;
+    // Since 3 is initialized as an integer, it the result after division is
+    // kept as an integer.
+    return 0;
+}
