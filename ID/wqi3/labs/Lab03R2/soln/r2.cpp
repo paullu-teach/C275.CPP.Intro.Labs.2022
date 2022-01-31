e021a8d729f5b7e3d70834c0cc99ebc11ce0a0b0
WillQizza <williamqipizza@gmail.com>
2022-01-31 16:15:41 -0700

    extract function from main method

--- ID/wqi3/labs/Lab03R2/soln/r2.cpp
+++ ID/wqi3/labs/Lab03R2/soln/r2.cpp
@@ -8,19 +8,30 @@
 
 using namespace std;
 
+/**
+ * @brief Finds B in the equation (A + B) / 2 = S given a and s
+ * 
+ * @param a a variable
+ * @param s s variable
+ * @return the b variable
+ */
+int findB(int a, int s);
+
 int main() {
     int a = 0;
-    int b = 0;
     int result = 0;
 
     // The original formula is (A + B) / 2 = result
     // we are given A and result
     cin >> a >> result;
 
-    // Solving for B given the formula gives us (result * 2) - a
-    b = (result * 2) - a;
-
     // Print out the missing variable
-    cout << b << endl;
+    cout << findB(a, result) << endl;
+    
     return 0;
+}
+
+int findB(int a, int s) {
+    // Solving for B given the formula gives us (result * 2) - a
+    return (s * 2) - a;
 }
