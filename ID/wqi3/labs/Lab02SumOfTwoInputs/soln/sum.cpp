4fbe9de765e8c097c63596c1278a23e5002ac362
WillQizza <williamqipizza@gmail.com>
2022-01-31 15:57:53 -0700

    lab 02 - sum of two inputs

---
+++ ID/wqi3/labs/Lab02SumOfTwoInputs/soln/sum.cpp
@@ -0,0 +1,20 @@
+/**
+ * Lab 2 - Sum of Two Inputs
+ * The purpose of this lab is to print the sum of the two integers given.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Take input
+    int x, y;
+    cin >> x >> y;
+
+    // Output the sum of the two variables
+    int result = x + y;
+    cout << result << endl;
+
+    return 0;
+}
