866a2fa8047f0189bc141ae4c3e15eb00f53e8d0
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2022-02-03 10:56:32 -0700

    Add files via upload

---
+++ ID/jdantas/kattis/R2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    // Task 3 (Lab 1) - R2
+    int R1, R2, S; // We initialize R1, R2 and S
+
+    cin >> R1 >> S; // We get the values of R1 and S from the users 
+    // in the terminal
+
+    R2 = ( 2 * S ) - R1; // This calculates the value of R2
+
+    cout << R2; // We output the value of R2
+    
+    return 0;
+}    
