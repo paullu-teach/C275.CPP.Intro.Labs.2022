f8013f94ba31237e580c620f0cd230fd4877eb72
Julia-Dantas <90366819+Julia-Dantas@users.noreply.github.com>
2022-02-03 11:02:19 -0700

    Update sum_of_two_inputs.cpp

--- ID/jdantas/your_tasks_lab_1_hello_world/sum_of_two_inputs.cpp
+++ ID/jdantas/your_tasks_lab_1_hello_world/sum_of_two_inputs.cpp
@@ -13,7 +13,7 @@ int main()
     cin >> number_1 >> number_2; // using cin, we ask the user to input two ints
     // in the terminal, one for each of the numbers we will be adding together
 
-    cout << number_1 + number_2; // we output the two numbers added together
+    cout << number_1 + number_2 << endl; // we output the two numbers added together
     
     return 0;
 }
