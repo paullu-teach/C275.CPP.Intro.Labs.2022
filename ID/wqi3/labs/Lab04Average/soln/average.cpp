f1d7f758ae58d5506f06942b70586b9da8a85e0a
WillQizza <williamqipizza@gmail.com>
2022-02-05 23:05:32 -0700

    lab 04

---
+++ ID/wqi3/labs/Lab04Average/soln/average.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int c;
+
+    cin >> a >> b >> c;
+    float average = (a + b + c) / 3.0;
+
+    cout << average << endl;
+    
+    return 0;
+}
