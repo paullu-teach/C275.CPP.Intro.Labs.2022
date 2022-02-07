742aa8971f5dbf1b8d7712fb76845acf28881f66
WillQizza <williamqipizza@gmail.com>
2022-02-06 22:29:28 -0700

    lab 25

---
+++ ID/wqi3/labs/Lab25Cups/README.md
@@ -0,0 +1,12 @@
+# Lab 25 - Stacking Cups
+
+This lab can be found [here](https://open.kattis.com/problems/cups) if you would like to give it a try yourself.
+
+## Goal
+
+The goal of this lab is to sort the cup names in order given their radius. However, some radiuses are doubled if the radius is given before the cup name.
+
+## Running
+
+- This lab can be ran using `make run`
+- The test cases defined for this lab can be ran using `make tests`
---
+++ ID/wqi3/labs/Lab25Cups/soln/cups.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int cups;
+    cin >> cups;
+    cin.ignore();   // clear out left over input in cin so getline can retrieve the actual line
+
+    // sort the cups by radius.
+    unordered_map<int, string> cupsByRadius;
+    vector<int> cupSizes;
+
+    // store all cups and their sizes
+    for (int i = 0; i < cups; i++) {
+        string input;
+        getline(cin, input);
+        
+        bool isMalformedRequest = isdigit(input[0]);    // malformed requests are requests such that the radius is returned first and is doubled
+        string leftPart = input.substr(0, input.find(" "));
+        string rightPart = input.substr(input.find(" ") + 1);
+
+        // determine which is the radius and colour
+        int radius;
+        string colour;
+        if (isMalformedRequest) {
+            radius = stoi(leftPart) / 2;
+            colour = rightPart;
+        } else {
+            radius = stoi(rightPart);
+            colour = leftPart;
+        }
+
+        // store cup sizes
+        cupsByRadius[radius] = colour;
+        cupSizes.push_back(radius);
+    }
+
+    // sort cupSizes
+    sort(cupSizes.begin(), cupSizes.end());
+
+    // output cup colours by cupSizes
+    for (int cupSize : cupSizes) {
+        cout << cupsByRadius[cupSize] << endl;
+    }
+    return 0;
+}
